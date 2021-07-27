ae6633e58f254a82625aa1adee67afeb0f08d1f8
Andrew <20448589@life.hkbu.edu.hk>
2021-07-27 21:11:33 +0800

    a

--- game.py
+++ game.py
@@ -29,6 +29,7 @@ def check_collision(pipes):
     for pipe in pipes:
         if bird_rect.colliderect(pipe):
             print('collision')
+            death_sound.play()
             return False
 
     if bird_rect.top <= -100 or bird_rect.bottom >= 900:
@@ -41,16 +42,47 @@ def rotate_bird(bird):
     new_bird = pygame.transform.rotozoom(bird, -bird_movement *3, 1)
     return new_bird
 
+def bird_animation():
+    new_bird = bird_frames[bird_index]
+    new_bird_rect = new_bird.get_rect(center = (100, bird_rect.centery))
+    return new_bird, new_bird_rect
 
+def score_display(game_state):
+    if game_state == 'main_game':
+        score_surface = game_font.render(str(int(score)), True, (255, 255, 255)) # white
+        score_rect = score_surface.get_rect(center = (288, 100))
+        screen.blit(score_surface, score_rect)
+    if game_state == 'game_over':
+        score_surface = game_font.render(f'Score: {int(score)}', True, (255, 255, 255)) # white
+        score_rect = score_surface.get_rect(center = (288, 100))
+        screen.blit(score_surface, score_rect)
+
+        high_score_surface = game_font.render(f'High score: {int(high_score)}', True, (255, 255, 255)) # white
+        high_score_rect = high_score_surface.get_rect(center = (288, 850))
+        screen.blit(high_score_surface, high_score_rect)
+
+def update_score(score, high_score):
+    if score > high_score:
+        high_score = score
+    return high_score
+
+
+
+pygame.mixer.pre_init(frequency= 44100, size=16, channels=1, buffer=512)
 pygame.init()
 screen = pygame.display.set_mode((576, 1024))
 clock = pygame.time.Clock()
+game_font = pygame.font.Font('04B_19.TTF', 40)
 
 # Game Var
 gravity = 0.25
 bird_movement = 0
 
 game_active = True
+
+score = 0
+high_score = 0
+
 
 
 
@@ -69,6 +101,9 @@ bird_index = 0
 bird_surface = bird_frames[bird_index]
 bird_rect = bird_surface.get_rect(center = (100,512))
 
+BIRDFLAP = pygame.USEREVENT + 1
+pygame.time.set_timer(BIRDFLAP, 200)
+
 
 # bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert_alpha()
 # bird_surface = pygame.transform.scale2x(bird_surface)
@@ -82,6 +117,16 @@ SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1200) #1.2
 pipe_height = [400, 600, 800]
 
+
+game_over_surface = pygame.transform.scale2x(pygame.image.load('assets/message.png').convert_alpha())
+game_over_rect = game_over_surface.get_rect(center = (288, 512))
+
+flap_sound = pygame.mixer.Sound('sound/sfx_wing.wav')
+death_sound = pygame.mixer.Sound('sound/sfx_hit.wav')
+score_sound = pygame.mixer.Sound('sound/sfx_point.wav')
+score_sound_countdown = 100
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -93,15 +138,24 @@ while True:
                 # print("flat")
                 bird_movement = 0
                 bird_movement -= 6
+                flap_sound.play()
 
             if event.key == pygame.K_SPACE and game_active == False:
                 game_active = True
                 pipe_list.clear()
                 bird_rect.center = (100, 512)
                 bird_movement = 0
+                score = 0
+
+        if event.type == BIRDFLAP:
+            if bird_index < 2:
+                bird_index += 1
+            else:
+                bird_index = 0
+
+            bird_surface, bird_rect = bird_animation()
 
 
-        
         if event.type == SPAWNPIPE:
             # print('pipe')
             pipe_list.extend(create_pipe())
@@ -119,7 +173,16 @@ while True:
         # Pipes
         pipe_list = move_pipes(pipe_list)
         draw_pipes(pipe_list)
-
+        score += 0.01
+        score_display('main_game')
+    else:
+        screen.blit(game_over_surface, game_over_rect)
+        high_score = update_score(score, high_score)
+        score_display('game_over')
+        score_sound_countdown -= 1
+        if score_sound_countdown <= 0:
+            score_sound.play()
+            score_sound_countdown = 100
 
 
     # Floor
