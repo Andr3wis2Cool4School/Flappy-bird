4f4cc1ca893688f051ca2bb71a1f3599cd07600e
Andrew <20448589@life.hkbu.edu.hk>
2021-07-27 19:23:53 +0800

    a

--- game.py
+++ game.py
@@ -1,11 +1,23 @@
 import pygame, sys
 
+def draw_floor():
+    screen.blit(floor_surface, (floor_x_pos, 900))
+    screen.blit(floor_surface, (floor_x_pos + 576, 900)) # 2 floor
+    
+
 pygame.init()
 screen = pygame.display.set_mode((576, 1024))
 clock = pygame.time.Clock()
 
-bg_surface = pygame.image.load('assets/background-day.png')
+bg_surface = pygame.image.load('assets/background-day.png').convert() # Make faster
 bg_surface = pygame.transform.scale2x(bg_surface) # X2
+
+floor_surface = pygame.image.load('assets/base.png').convert()
+floor_surface = pygame.transform.scale2x(floor_surface)
+floor_x_pos = 0
+
+bird_surface = pygame.image.load('assets/')
+
 
 while True:
     for event in pygame.event.get():
@@ -14,6 +26,13 @@ while True:
             sys.exit()
 
     screen.blit(bg_surface, (0,0))
+    floor_x_pos -= 1
+    # screen.blit(floor_surface, (floor_x_pos, 900))
+    draw_floor()
+    if floor_x_pos <= -576:
+        floor_x_pos = 0
+
+    
 
     pygame.display.update()
     clock.tick(120)
