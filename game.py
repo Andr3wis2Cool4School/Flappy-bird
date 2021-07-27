e7594e88b3352e0334be78f162f9002889e9a60e
Andrew <20448589@life.hkbu.edu.hk>
2021-07-27 19:33:05 +0800

    a

--- game.py
+++ game.py
@@ -9,6 +9,12 @@ pygame.init()
 screen = pygame.display.set_mode((576, 1024))
 clock = pygame.time.Clock()
 
+# Game Var
+gravity = 0.25
+bird_movement = 0
+
+
+
 bg_surface = pygame.image.load('assets/background-day.png').convert() # Make faster
 bg_surface = pygame.transform.scale2x(bg_surface) # X2
 
@@ -16,7 +22,9 @@ floor_surface = pygame.image.load('assets/base.png').convert()
 floor_surface = pygame.transform.scale2x(floor_surface)
 floor_x_pos = 0
 
-bird_surface = pygame.image.load('assets/')
+bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert()
+bird_surface = pygame.transform.scale2x(bird_surface)
+bird_rect = bird_surface.get_rect(center = (100, 512))
 
 
 while True:
@@ -25,7 +33,15 @@ while True:
             pygame.quit()
             sys.exit()
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print("flat")
+
     screen.blit(bg_surface, (0,0))
+
+    bird_movement += gravity
+    bird_rect.centery += bird_movement
+    screen.blit(bird_surface, bird_rect)
     floor_x_pos -= 1
     # screen.blit(floor_surface, (floor_x_pos, 900))
     draw_floor()
